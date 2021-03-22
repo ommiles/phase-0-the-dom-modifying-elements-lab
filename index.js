83d22a73c3f19cab3246250ab7e7901895376119
OM Miles <olivia.m.miles@gmail.com>
2021-03-21 17:44:20 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById(`main`)
+main.remove()
+
+const newHeader = document.createElement(`h1`)
+document.body.appendChild(newHeader)
+
+newHeader.setAttribute(`id`, `victory`)
+
+const node = document.createTextNode(`Miles is the champion`)
+newHeader.appendChild(node)
